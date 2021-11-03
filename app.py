cb84f5dcc116a24eee722dcf57e3a53289c7cb51
Sunflower-777 <>
2021-11-03 18:51:35 +0900

    web表示

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
-import flask
+from flask import Flask, request, render_template
+
+app= Flask(__name__)
+
+
+@app.route('/')
+def optimize_task():
+  comment = "テスト表示だよ"
+  return render_template('index.html', comment=comment)
+
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
