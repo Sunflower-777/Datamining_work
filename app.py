0f1f3df69c6f74fa8f0b45f0faae0a2ff6da9aae
Sunflower-777 <>
2021-11-03 22:28:45 +0900

    データを受け取る機構を作った

--- app.py
+++ app.py
@@ -4,11 +4,17 @@ app= Flask(__name__)
 
 
 @app.route('/')
-def optimize_task():
+def index():
   comment = "テスト表示だよ"
   return render_template('index.html', comment=comment)
 
-
+@app.route('/result', methods=['POST'])
+def optimize_task():
+  if request.method == 'POST':
+    data = request.form.getlist("deadLine")
+    print(data)
+    comment = "テスト表示だよ"
+  return render_template('index.html', comment=comment)
 
 if __name__ == '__main__':
   app.run(debug=True)
